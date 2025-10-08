68188b6c906d56560694de833777033cef1a1496
0xtakemichi <fjerez034@gmail.com>
2025-10-07 22:30:50 -0300

    02 | Advanced Objects - Abstract classes

--- intermediate/03-advanced-classes.js
+++ intermediate/03-advanced-classes.js
@@ -17,3 +17,21 @@ person.sayAge = function () {
   console.log(`Tengo ${this.age} años`);
 };
 person.sayAge();
+
+// Clases abstractas
+
+class Animal {
+  constructor(name) {
+    if (new.target === Animal) {
+      throw new Error("No se puede instanciar una clase abstracta");
+    }
+    this.name = name;
+  }
+  makeSound() {
+    throw new Error("Este método debe ser implementado por la subclase");
+  }
+}
+
+// Error
+// const animal = new Animal("Animal");
+// console.log(animal);
