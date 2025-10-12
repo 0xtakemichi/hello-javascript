fd4007f39c856fbf6b3134e211947e5dbc3ea733
0xtakemichi <fjerez034@gmail.com>
2025-10-11 23:32:04 -0300

    04 | Async - Introduction and Event Loop

---
+++ intermediate/04-async.js
@@ -0,0 +1,20 @@
+// Programación Asíncrona
+
+// Código sincrono
+console.log("Inicio");
+for (let i = 0; i < 1000000000; i++) {}
+console.log("Fin");
+
+// Event Loop (Bucle de eventos)
+
+// Componentes del Event Loop:
+// 1. Call Stack (Pila de ejecución)
+// 2. Web APIs (APIs del navegador) o Node.js:
+// 3. Task Queue (setTimeout()) y Microtask Queue (Promesas)
+
+// Flujo del Event Loop:
+// 1. Call Stack
+// 2. Operaciones asíncronas -> Web APIs o Node.js
+// 3. Operación termina -> La coloca en Task Queue o Microtask Queue
+// 4. Si Call Stack vacío -> Mueve tareas del Microtask Queue o Task Queue al Call Stack
+// 5. El proceso se repite
