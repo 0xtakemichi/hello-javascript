0687b8197d6f6f8040837990817a36f8183badd7
0xtakemichi <fjerez034@gmail.com>
2025-10-12 23:24:40 -0300

    04 | Async - Callbacks and Callback Hell

--- intermediate/04-async.js
+++ intermediate/04-async.js
@@ -18,3 +18,39 @@ console.log("Fin");
 // 3. Operación termina -> La coloca en Task Queue o Microtask Queue
 // 4. Si Call Stack vacío -> Mueve tareas del Microtask Queue o Task Queue al Call Stack
 // 5. El proceso se repite
+
+// Código Asíncrono
+
+// - Callbacks
+
+console.log("Start");
+setTimeout(() => console.log("Ejecución de setTimeout"), 2000);
+console.log("End");
+
+// - Problemas: Callback Hell
+
+function step1(callback) {
+  setTimeout(() => {
+    console.log("Paso 1 completado");
+    callback();
+  }, 1000);
+}
+function step2(callback) {
+  setTimeout(() => {
+    console.log("Paso 2 completado");
+    callback();
+  }, 1000);
+}
+function step3(callback) {
+  setTimeout(() => {
+    console.log("Paso 3 completado");
+    callback();
+  }, 1000);
+}
+step1(() => {
+  step2(() => {
+    step3(() => {
+      console.log("Todos los pasos completados");
+    });
+  });
+});
