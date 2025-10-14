ce55d2543878304e8c9a1b892e6aa73555ad6c9c
0xtakemichi <fjerez034@gmail.com>
2025-10-13 23:11:27 -0300

    05 | APIs - Introduction and HTTP Methods

---
+++ intermediate/05-apis.js
@@ -0,0 +1,22 @@
+// Manejo de APIs
+
+// - APIs REST (HTTP + URLs + JSON)
+
+// Métodos HTTP
+// - GET
+// - POST
+// - PUT
+// - DELETE
+
+// Códigos de respiesta HTTP
+// 2xx - Lado del servidor
+// 200 - OK
+// 201 - Created
+// 204 - No Content
+// 4xx - Lado del cliente
+// 400 - Bad Request
+// 401 - Unauthorized
+// 403 - Forbidden
+// 404 - Not Found
+// 5xx - Lado del servidor
+// 500 - Internal Server Error
