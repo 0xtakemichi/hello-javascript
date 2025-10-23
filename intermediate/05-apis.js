59c1374b59a4e23c2c0a9875f4a04c8e1bdbd29e
0xtakemichi <fjerez034@gmail.com>
2025-10-22 23:46:33 -0300

    05 | APIs - Fetch with Async/Await

--- intermediate/05-apis.js
+++ intermediate/05-apis.js
@@ -36,3 +36,17 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     // Maneja el error
     console.log(error);
   });
+
+// Uso de Async/Await
+
+async function getData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData();
