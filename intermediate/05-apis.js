ca7de188c139a9e745a2cf95bfa64de7eacf486c
0xtakemichi <fjerez034@gmail.com>
2025-10-27 22:39:57 -0300

    05 | APIs - Public APIs Example (PokeAPI)

--- intermediate/05-apis.js
+++ intermediate/05-apis.js
@@ -141,3 +141,23 @@ getWeather("Santiago");
 // Versionado de APIs
 // - https://api.example.com/v1/resources
 // - https://api.example.com/v2/resources
+
+// Otras APIs
+
+async function getPokemon(pokemon) {
+  // https://pokeapi.co
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(`Habilidades de ${data.name}`);
+    data.abilities.forEach((ability) => {
+      console.log(ability.ability.name);
+    });
+  } catch (error) {
+    console.log("Error", error);
+  }
+}
+
+getPokemon("pikachu");
