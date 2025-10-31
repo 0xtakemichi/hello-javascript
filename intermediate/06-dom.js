72e168c8f7037b032db0a4d2b10218d65ef44e81
0xtakemichi <fjerez034@gmail.com>
2025-10-30 21:38:49 -0300

    06 | DOM - Element Creation and Removal

--- intermediate/06-dom.js
+++ intermediate/06-dom.js
@@ -50,3 +50,33 @@ const button = document.querySelector("button");
 button.style.backgroundColor = "blue";
 button.style.color = "white";
 button.style.padding = "10px";
+
+// - Creación y eliminación de elementos
+
+// Creación
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "Nuevo párrafo";
+newParagraph.style.padding = "8px";
+
+container.appendChild(newParagraph);
+
+const itemsList = document.querySelector("ul");
+const newItem = document.createElement("li");
+newItem.textContent = "Nuevo ítem";
+
+// Inserción en una posición específica
+const secondItem = itemsList.children[1];
+itemsList.insertBefore(newItem, secondItem);
+
+itemsList.append(newItem);
+itemsList.prepend(newItem);
+secondItem.before(newItem);
+secondItem.after(newItem);
+
+// Eliminación
+
+newParagraph.remove();
+
+// Eliminación usando el padre
+const parent = newParagraph.parentElement;
+parent.removeChild(newParagraph);
