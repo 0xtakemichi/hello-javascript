db9ff6855ca116b148edce034d77e3acd45e90b1
0xtakemichi <fjerez034@gmail.com>
2025-11-03 22:16:20 -0300

    10 | Regular Expressions - Replace Method

--- intermediate/10-regex.js
+++ intermediate/10-regex.js
@@ -12,9 +12,15 @@ const text = "Pastel de Choclo con abc";
 console.log(regex.test(text));
 console.log(regex2.test(text));
 
-const regex3 = /\d/;
+const regex3 = /\d/g;
 const regex4 = /[3-5]/;
 const text2 = "mi edad es de 15";
 
 console.log(regex3.test(text2));
 console.log(regex4.test(text2));
+
+// replace (reemplaza el texto que coincida con la patrón)
+
+const regex5 = /JavaScript/;
+console.log("Hola JavaScript".replace(regex5, "JS"));
+console.log("Contando: 1 2 3 4 5 6 7".replace(regex3, "[X]"));
